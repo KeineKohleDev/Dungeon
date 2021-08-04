26dcc79626dd964506e708e5659293fa68852e88
KeineKohleDev <keinekohledevf@gmail.com>
2021-08-04 18:51:47 +0200

    code improvement

--- src/main/java/me/keinekohle/net/listeners/lobby/ListenerLobbyInventoryClickEvent.java
+++ src/main/java/me/keinekohle/net/listeners/lobby/ListenerLobbyInventoryClickEvent.java
@@ -21,41 +21,57 @@ public class ListenerLobbyInventoryClickEvent implements Listener {
     public void onClick(InventoryClickEvent event) {
         if (event.getWhoClicked() instanceof Player player && !KeineKohle.INGAME && !KeineKohle.BUILDMODE.contains(player)) {
             event.setCancelled(true);
-            if(event.getCurrentItem() != null) {
+            if (event.getCurrentItem() != null) {
                 ItemStack clickedItem = event.getCurrentItem();
                 if (event.getView().getTitle().equals(GlobalUtilities.getColorByName(KeineKohle.CHESTDISPLAYNAME) + KeineKohle.CHESTDISPLAYNAME)) {
-                    if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
-                        ItemMeta clickedItemMeta = clickedItem.getItemMeta();
-                        String itemDisplayname = clickedItemMeta.getDisplayName();
-                        if (itemDisplayname.equals(GlobalUtilities.getColorByName(Classes.SHOPCLASSES) + Classes.SHOPCLASSES)) {
-                            player.openInventory(InventoryUtilities.createClassesInventory(player));
-                        }
-                    }
+                    handleShopInventorActions(player, clickedItem);
                 } else if (event.getView().getTitle().equals(GlobalUtilities.getColorByName(Classes.SHOPCLASSES) + Classes.SHOPCLASSES)) {
-                    if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
-                        if (event.getClick() == ClickType.LEFT && clickedItem.getItemMeta().hasLore() && clickedItem.getItemMeta().getLore().contains(InventoryUtilities.PREVIEW)) {
-                            MySQLMethods mySQLMethods = new MySQLMethods();
-                            KeineKohle.INPRIVIEW.add(player);
-                            PlayerUtilities.clearPlayerInventory(player);
-                            mySQLMethods.giveClassItems(player, GlobalUtilities.getNameWithoutColorCode(clickedItem.getItemMeta().getDisplayName()), 1);
-                            player.sendMessage(KeineKohle.PREFIX + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "If you move, the preview will stop.");
-                            player.closeInventory();
-                        } else if (event.getClick() == ClickType.RIGHT) {
-                            MySQLMethods mySQLMethods = new MySQLMethods();
-                            String className = GlobalUtilities.getNameWithoutColorCode(clickedItem.getItemMeta().getDisplayName());
-                            int playerCoins = mySQLMethods.selectCoinsFromPlayerUUID(player);
-                            int classCoast = mySQLMethods.selectClassCoastFromClasses(className, 1);
-                            if(playerCoins >= classCoast) {
-                                mySQLMethods.updatePlayerCoins(player, (playerCoins-classCoast));
-                                mySQLMethods.giveClassAccessToPlayer(player, className);
-                                player.sendMessage(KeineKohle.PREFIX + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "You purchased the class " + clickedItem.getItemMeta().getDisplayName() + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "for " + classCoast + " coins!");
-                                player.openInventory(InventoryUtilities.createClassesInventory(player));
-                                LobbyScoreboard.sendLobbyScoreboard(player);
-                            }
-                        }
-                    }
+                    handleClassesInventoryActions(event, player, clickedItem);
                 }
             }
+        }
+    }
+
+    private void handleShopInventorActions(Player player, ItemStack clickedItem) {
+        if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
+            ItemMeta clickedItemMeta = clickedItem.getItemMeta();
+            String itemDisplayname = clickedItemMeta.getDisplayName();
+            if (itemDisplayname.equals(GlobalUtilities.getColorByName(Classes.SHOPCLASSES) + Classes.SHOPCLASSES)) {
+                player.openInventory(InventoryUtilities.createClassesInventory(player));
+            }
+        }
+    }
+
+    private void handleClassesInventoryActions(InventoryClickEvent event, Player player, ItemStack clickedItem) {
+        if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
+            if (event.getClick() == ClickType.LEFT && clickedItem.getItemMeta().hasLore() && clickedItem.getItemMeta().getLore().contains(InventoryUtilities.PREVIEW)) {
+                previewClass(player, clickedItem);
+            } else if (event.getClick() == ClickType.RIGHT) {
+                purchaseClass(player, clickedItem);
+            }
+        }
+    }
+
+    private void previewClass(Player player, ItemStack clickedItem) {
+        MySQLMethods mySQLMethods = new MySQLMethods();
+        KeineKohle.INPRIVIEW.add(player);
+        PlayerUtilities.clearPlayerInventory(player);
+        mySQLMethods.giveClassItems(player, GlobalUtilities.getNameWithoutColorCode(clickedItem.getItemMeta().getDisplayName()), 1);
+        player.sendMessage(KeineKohle.PREFIX + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "If you move, the preview will stop.");
+        player.closeInventory();
+    }
+
+    private void purchaseClass(Player player, ItemStack clickedItem) {
+        MySQLMethods mySQLMethods = new MySQLMethods();
+        String className = GlobalUtilities.getNameWithoutColorCode(clickedItem.getItemMeta().getDisplayName());
+        int playerCoins = mySQLMethods.selectCoinsFromPlayerUUID(player);
+        int classCoast = mySQLMethods.selectClassCoastFromClasses(className, 1);
+        if (playerCoins >= classCoast) {
+            mySQLMethods.updatePlayerCoins(player, (playerCoins - classCoast));
+            mySQLMethods.giveClassAccessToPlayer(player, className);
+            player.sendMessage(KeineKohle.PREFIX + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "You purchased the class " + clickedItem.getItemMeta().getDisplayName() + GlobalUtilities.getColorByName(KeineKohle.CHATCOLOR) + " " + "for " + classCoast + " coins!");
+            player.openInventory(InventoryUtilities.createClassesInventory(player));
+            LobbyScoreboard.sendLobbyScoreboard(player);
         }
     }
 }
